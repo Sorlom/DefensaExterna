d787e6541c02facec94d4c93792dc69479560a0c
Sorlom <jsolanor1994@gmail.com>
2018-01-26 01:49:50 -0400

    Creacion de Venta

---
+++ ProDefExterna/SistemaVentaPrestamo/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaVentaPrestamo.Models;
+using System.Web.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SistemaVentaPrestamo.Controllers
+{
+    public class LoginController : Controller
+    {
+        private BDDEFEXTEntities db = new BDDEFEXTEntities();
+
+        // GET: Login
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            if (TempData["DErr"] != null)
+            {
+                ViewBag.Msg = TempData["DErr"].ToString();
+            }
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Index(FormCollection fc)
+        {
+
+            string acc = fc["Nom"].ToString();
+            string pass = GetMD5(fc["Pass"].ToString());
+            TempData["Login"] = acc;
+
+            var count = db.Personal.Where(x => x.Login == acc && x.Password == pass).Count();
+
+            if (count == 0)
+            {
+                ViewBag.Msg = "Usuario Invalido";
+                return View();
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(acc, false);
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        [AllowAnonymous]
+        public ActionResult Logout()
+        {
+            TempData["Login"] = "";
+            TempData["IDlogin"] = "";
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+        public string GetMD5(string str)
+        {
+            MD5 md5 = MD5CryptoServiceProvider.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = md5.ComputeHash(encoding.GetBytes(str));
+            for (int i = 0; i < stream.Length; i++) sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+    }
+}
---
+++ ProDefExterna/SistemaVentaPrestamo/Controllers/PersonalController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaVentaPrestamo.Models;
+
+namespace SistemaVentaPrestamo.Controllers
+{
+    public class PersonalController : Controller
+    {
+        private BDDEFEXTEntities db = new BDDEFEXTEntities();
+
+        // GET: Personal
+        public ActionResult Index()
+        {
+            return View(db.Personal.ToList());
+        }
+
+        // GET: Personal/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personal personal = db.Personal.Find(id);
+            if (personal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personal);
+        }
+
+        // GET: Personal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personal/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Login,Password,nombreCompleto,CI,Celular,Email")] Personal personal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Personal.Add(personal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personal);
+        }
+
+        // GET: Personal/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personal personal = db.Personal.Find(id);
+            if (personal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personal);
+        }
+
+        // POST: Personal/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Login,Password,nombreCompleto,CI,Celular,Email")] Personal personal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personal);
+        }
+
+        // GET: Personal/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personal personal = db.Personal.Find(id);
+            if (personal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personal);
+        }
+
+        // POST: Personal/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Personal personal = db.Personal.Find(id);
+            db.Personal.Remove(personal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
